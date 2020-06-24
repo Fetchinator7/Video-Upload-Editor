ca1e0b6f27bcc0c99bbe622b087e778562a6ab45
Luke Johnson <lukejohnson3712@gmail.com>
2020-06-23 23:40:55 -0500

    Add required index.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+const storeInstance = createStore(
+  // combineReducers({
+  //   arr: arrReducer
+  // }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
