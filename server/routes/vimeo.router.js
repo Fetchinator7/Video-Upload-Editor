b40409b533edb36b7353fc4e58ddcccbbb263658
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-29 20:30:14 -0500

    Add basic vimeo get path.

--- server/routes/vimeo.router.js
+++ server/routes/vimeo.router.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const accessToken = process.env.ACCESS_TOKEN;
+console.log(clientID, clientSecret, accessToken);
+
 router.post('/', (req, res) => {
   // const videoTitle = req.body.videoTitle;
   res.sendStatus(200);
+});
+
+const Vimeo = require('vimeo').Vimeo;
+const client = new Vimeo(clientID, clientSecret, accessToken);
+
+client.request({
+  method: 'GET',
+  path: '/tutorial'
+}, function (error, body, statusCode, headers) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(body);
 });
 
 module.exports = router;
