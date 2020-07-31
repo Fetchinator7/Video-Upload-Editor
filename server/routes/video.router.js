f5f99208a6327639c6b33b969157b6381e88ac72
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-30 22:36:16 -0500

    After posting a video post it to Vimeo.

--- server/routes/video.router.js
+++ server/routes/video.router.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const moment = require('moment');
 const { spawn } = require('child_process');
 
 const mainOutputFolder = process.env.MAIN_OUTPUT_FOLDER;
 
 router.post('/', (req, res) => {
-  const userName = req.body.userName;
-  const videoTitle = req.body.videoTitle;
   const videoPath = req.body.videoPath;
+  const title = req.body.title;
+  const userName = req.body.userName;
+  const description = req.body.description;
   console.log('post to video');
   if (userName !== undefined) {
-    const pyProcess = spawn('python3', ['server/dependencies/local_operations.py', mainOutputFolder, videoPath, videoTitle, userName]);
+    // TODO Handle error correctly.
+    const pyProcess = spawn('python3', ['server/dependencies/local_operations.py', mainOutputFolder, videoPath, title, userName]);
     pyProcess.stdout.on('data', (data) => {
-      console.log(data.toString());
-      res.status(200).send({ output: data.toString() });
+      // The data object is what's sent to the python console which has the messages and the
+      // output path in {}:
+      // {
+      //   the raw output is: "New directory, \".../New Title\" was created!\nSession log created at \"...New Title-log.txt\"\nOpened file/folder: \"..." with the default application.\n{/...New Title.mp4}\n"
+      //   the path is in curly braces then extracted to be: "/...New Title.mp4"
+      //   without the path is: "New directory, \".../New Title\" was created!\nSession log created at \"...New Title-log.txt\"\nOpened file/folder: \"..." with the default application."
+      // }
+      const output = data.toString().replace(/{(.*?)}/, '').slice(0, -2);
+      const outputVideoPath = data.toString().match(/{(.*?)}/) ? data.toString().match(/{(.*?)}/)[1] : '';
+      const bodyObj = {
+        videoPath: outputVideoPath,
+        title: moment().format('MM-DD-yyyy') + ' ' + title,
+        description: description ? description : ''
+      };
+      // http://localhost:5000/vimeo
+      axios.post('/vimeo', bodyObj).then(() => {
+        res.status(200).send({ output: output, path: outputVideoPath });
+      }).catch(err => {
+        console.log('err', err);
+        res.sendStatus(500);
+      });
+      // res.sendStatus(200)
     });
     pyProcess.stderr.on('data', (data) => {
       console.log(data.toString());
