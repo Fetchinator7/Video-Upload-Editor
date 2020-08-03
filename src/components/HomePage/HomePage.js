c32b736e297a3e69840e69733622fc1b73c8aad9
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-02 20:21:40 -0500

    Multiple video dialogue.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,9 +6,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControl from '@material-ui/core/FormControl';
-import { Button, Dialog, DialogContent, DialogContentText, createMuiTheme, MuiThemeProvider, DialogTitle, DialogActions, TextField } from '@material-ui/core';
+import { Button, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import UsersJsonFile from './users.json';
-import visibilityOptions from '../VideosTable/visibilityOptions.json';
 import VideosTable from '../VideosTable/VideosTable';
 
 const useStyles = createMuiTheme(
@@ -16,27 +15,12 @@ const useStyles = createMuiTheme(
 );
 
 class HomePage extends Component {
-  state = {
-    dialogueOpen: false,
-    dialogueText: 'text silly',
-    visibilityLevelOpen: false,
-    visibilityLevel: 'anybody',
-    showTextField: false,
-    password: ''
-  }
-
-  showPasswordField = (bool) => {
-    this.setState({ showTextField: bool })
-  }
-
   render() {
     return (
       <>
         <MuiThemeProvider theme={useStyles}>
           <header className='App-header'>
             <h1>Homepage</h1>
-            <h1>visibility Level: {this.state.visibilityLevel}</h1>
-            <h1>{(this.state.visibilityLevel === 'password') && `Password: ${this.state.password}`}</h1>
           </header>
           Select a user:
           <FormControl component='fieldset'>
@@ -56,92 +40,22 @@ class HomePage extends Component {
                 <FormControlLabel value={user} control={<Radio />} label={user} key={`user-options-${index}`} />
               )}
             </RadioGroup>
-            <Dialog
-              disableBackdropClick
-              disableEscapeKeyDown
-              maxWidth="xs"
-              // onEntering={handleEntering}
-              aria-labelledby="confirmation-dialog-title"
-              open={this.state.visibilityLevelOpen}
-            >
-              <DialogTitle id="confirmation-dialog-title">Phone Ringtone</DialogTitle>
-              <DialogContent dividers>
-                <RadioGroup
-                  // ref={radioGroupRef}
-                  aria-label="ringtone"
-                  name="ringtone"
-                  value={this.state.visibilityLevel}
-                  onChange={event => this.setState({ visibilityLevel: event.target.value })}
-                >
-                  {visibilityOptions.visibility.map((option) => {
-                    if (option.title === this.state.visibilityLevel) {
-                      if (this.state.showTextField !== option.password) {
-                        this.setState({ showTextField: option.password })
-                      }
-                    }
-                    return <FormControlLabel value={option.title} key={option.title} control={<Radio />} label={option.title} />
-                  })}
-                </RadioGroup>
-              {/* Dialogue that prompts the user to send a message for a new user request. */}
-              {this.state.showTextField &&
-                <TextField
-                  focused
-                  autoFocus
-                  margin="dense"
-                  label="Password:"
-                  type="text"
-                  fullWidth
-                  maxLength='10'
-                  value={this.state.message}
-                  onChange={
-                    // Set maximum number of message characters to 1000.
-                    event => event.target.value.length <= 1000 && this.setState({ password: event.target.value })
-                  }
-                />
-              }
-              </DialogContent>
-              <DialogActions>
-                <Button
-                  onClick={() => {
-                    this.state.password && this.setState({ visibilityLevelOpen: false })
-                  }}
-                  color="primary"
-                >
-                  Ok
-                </Button>
-              </DialogActions>
-            </Dialog>
             <Button
               variant='contained'
               color='primary'
-              onClick={() => this.setState({ visibilityLevelOpen: !this.state.visibilityLevelOpen })}
-            >
-              visibility
-          </Button>
-            <Button
-              variant='contained'
-              color='primary'
-              disabled={this.props.videos.length > 0}
               onClick={() => this.props.dispatch({ type: 'OPEN_PYTHON_FILE_PICKER' })}
             >
               Add File
-          </Button>
-            <Button
-              variant='contained'
-              color='primary'
-              onClick={() => this.setState({ dialogueOpen: true })}
-            >
-              Open
-          </Button>
+            </Button>
           </FormControl>
           <VideosTable />
           <Button
             variant='contained'
             color='primary'
             disabled={(!this.props.user || this.props.videos.length === 0)}
-            onClick={() =>
+            onClick={() => {
+              this.props.dispatch({ type: 'DISABLE_EDITING' });
               this.props.videos.map((videoObj, index) => {
-                this.props.dispatch({ type: 'DISABLE_EDITING' });
                 this.props.dispatch({
                   type: 'UPLOAD_VIDEO_TO_VIMEO_AND_ARCHIVE_SOURCE',
                   payload: {
@@ -150,21 +64,15 @@ class HomePage extends Component {
                     description: videoObj.description,
                     userName: this.props.user
                   },
-                  visibility: this.state.visibilityLevel,
+                  visibility: videoObj.visibilityLevel,
                   index: index,
-                  password: this.state.password
+                  password: videoObj.password
                 });
-              })}
+              });
+            }}
           >
             Upload Video(s)
-        </Button>
-          <Dialog open={this.state.dialogueOpen}>
-            <DialogContent>
-              <DialogContentText>
-                {this.state.dialogueText}
-              </DialogContentText>
-            </DialogContent>
-          </Dialog>
+          </Button>
         </MuiThemeProvider>
       </>
     );
