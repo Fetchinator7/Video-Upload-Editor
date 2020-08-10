4c713caca7150d132d68c61bcb39f9319c2635f1
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-09 21:10:15 -0500

    Remove unused resources.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
 import HomePage from '../HomePage/HomePage';
-import VideoPlayer from '../VideoPlayer/VideoPlayer';
-import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Router>
-          <ul className='nav'>
-            <li>
-              <NavLink exact to='/' activeClassName='active'>HOME</NavLink>
-            </li>
-            <li>
-              <NavLink exact to='/video-player' activeClassName='active'>VIDEO PLAYER</NavLink>
-            </li>
-          </ul>
-          <Route exact path='/'>
-            <HomePage />
-          </Route>
-          <Route exact path='/video-player'>
-            <VideoPlayer />
-          </Route>
-        </Router>
-      </>
+      <HomePage />
     );
   }
 }
